0e21bcdec4dd161909d8ded709b0adc4aeab70c2
jperalta <jperalta.henri@gmail.com>
2018-11-21 08:30:24 -0500

    added furniture db

--- db/mellow-database/mellow.sql
+++ db/mellow-database/mellow.sql
@@ -4,10 +4,13 @@ INSERT INTO rooms (type, url) VALUES
   'https://www.millerprop.com/wp-content/uploads/sites/4824/2016/03/DuvalCourt-Floorplan-Studio.jpg'
 );
 
-INSERT INTO studio_furn (type, url, rooms_id) VALUES
+INSERT INTO furniture (room, couch_url, bed_url, table_url, cabinet_url, rooms_id) VALUES
 (
-  'Bed',
+  'Studio',
+  'https://static.thenounproject.com/png/1106557-200.png',
   'https://b3h2.scene7.com/is/image/BedBathandBeyond/9446313300685m',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxWnaIeMARA0srI0--SrxPP-YB1swXrE0DYgm7N_6PBR1ysC-wRw',
+  'https://image.shutterstock.com/image-vector/medicine-cabinet-vector-icon-isolated-260nw-1152728507.jpg',
   1
 );
 
