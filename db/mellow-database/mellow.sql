f70c686624489945d4a5eee87a8d45ef60abcde5
jperalta <jperalta.henri@gmail.com>
2018-11-23 19:46:28 -0500

    bottom nav alignment

--- db/mellow-database/mellow.sql
+++ db/mellow-database/mellow.sql
@@ -6,12 +6,16 @@ INSERT INTO rooms (type, title, url, description) VALUES
   'This is how your items fit best in your 135 square-foot U-shaped room.'
 );
 
-INSERT INTO furniture (room, couch_url, bed_url, table_url, cabinet_url, rooms_id) VALUES
+INSERT INTO furniture (room, couch_name, couch_url, bed_name, bed_url, table_name, table_url, cabinet_name, cabinet_url, rooms_id) VALUES
 (
   'Bedroom',
+  'Couch',
   'https://static.thenounproject.com/png/1106557-200.png',
+  'Bed',
   'https://b3h2.scene7.com/is/image/BedBathandBeyond/9446313300685m',
+  'Table',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxWnaIeMARA0srI0--SrxPP-YB1swXrE0DYgm7N_6PBR1ysC-wRw',
+  'Cabinet',
   'https://image.shutterstock.com/image-vector/medicine-cabinet-vector-icon-isolated-260nw-1152728507.jpg',
   1
 );
