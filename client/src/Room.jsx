daf24be0ea6f206893a52bfc31a6143220ceb898
jperalta <jperalta.henri@gmail.com>
2018-12-01 15:14:20 -0500

    css to roomlist

--- client/src/Room.jsx
+++ client/src/Room.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import FooterNav from './FooterNav';
 import Add from "@material-ui/icons/Add";
-
+import AddCircle from '@material-ui/icons/AddCircle';
+import Chip from '@material-ui/core/Chip';
   // <img src={props.explore.url} />
-const Room = (props) => {
+	// {props.room.type}
+class Room extends Component{
+	state = {
+		show: false
+	}
+
+	toggleRoom = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  }
+	render(){
 	return(
 		<>
-			<Link to={`/rooms/${props.room.id}`}>
+
+
 			<div className="room-inlist">
-      		<h2>{props.room.type}</h2>
-					<div className="forward-arrow">{<Add />}</div>
+      		<h2> My Rooms</h2>
+					<div className="forward-arrow" onClick={this.toggleRoom.bind(this)}>
+					<Chip
+						avatar={<AddCircle />}
+        		label="Add Room"
+      		/>
+					</div>
   		</div>
-			</Link>
+
+				{this.state.show&&
+					<Link to={`/rooms/${this.props.room.id}`}>
+				<div className="add-room">
+					<img src="https://i.imgur.com/gvHk5Vq.png" alt="add bedroom"/>
+				</div>
+					</Link>
+				}
+
+
 			<FooterNav/>
 		</>
 	)
+}
 }
 
 export default Room;
